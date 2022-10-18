e8a5360fa9bd602e5e82761fb3289e24f4597c26
BruhNav <pranav20100@iiitnr.edu.in>
2022-10-18 22:11:22 +0530

    second commit

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import * as Mui from '@mui/material';
 import React from "react";
-<<<<<<< HEAD
 import Web3 from 'web3';
+    
 
 const Navbar =()=>{
+    
 
     var web3 = new Web3(Web3.givenProvider||"ws://localhost:8545");
 
@@ -11,11 +12,6 @@ const Navbar =()=>{
         e.preventDefault();
         web3.eth.requestAccounts().then(console.log);
     }
-    
-=======
-
-const Navbar =()=>{
->>>>>>> f8f25b7f7dca2870de03aa7c89de58f678e125e3
     return(
         <nav className="navbar">
             <div className="navbar links">
@@ -26,10 +22,7 @@ const Navbar =()=>{
                     <div className="navbarlinks floatLeft"><button className='leftLine'><a href="">Solutions</a></button></div>
                     <div className="navbarlinks floatLeft"><button className='leftLine'><a href="">Resources</a></button></div>
                     <div className="navbarlinks floatLeft"><button className='leftLine'><a href="">Contact</a></button></div>
-<<<<<<< HEAD
                     <div className="navbarlinks floatLeft"><button onClick={getWalletAddress} className='leftLine'><a href="">Get NFT</a></button></div>
-=======
->>>>>>> f8f25b7f7dca2870de03aa7c89de58f678e125e3
                 </div>
             </div>
         </nav>
